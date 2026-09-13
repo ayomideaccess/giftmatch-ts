616be9fd3c1fd43ab660e56e3bd204f417f620ed
Akinniyi Ayomide <accessayomide@gmail.com>
2026-09-13 17:53:51 +0100

    fix gmail smtp connection

--- src/services/email.service.ts
+++ src/services/email.service.ts
@@ -4,7 +4,9 @@ import nodemailer from 'nodemailer';
 dotenv.config();
 
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: 'smtp.gmail.com',
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.GMAIL_USER,
     pass: process.env.GMAIL_PASS,
